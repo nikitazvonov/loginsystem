82e84dbf74750fe55ea245eeb6b8e0706eab582b
nikita <zvonovnikita42@gmail.com>
2023-08-07 16:47:43 +0700

    initialization

---
+++ includes/dbh.php
@@ -0,0 +1,15 @@
+<?php
+
+$host = 'localhost';
+$dbname = 'loginsystem';
+$dbuser = 'root';
+$dbpassword = '386342';
+
+$dsn = "mysql:host=$host;dbname=$dbname";
+
+try {
+    $pdo = new PDO($dsn, $dbuser, $dbpassword);
+    $pdo->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
+} catch (PDOException $e) {
+    die($e->getMessage());
+};
